b06cf1c28b8fa585e24433f71d08a0492a39469d
IlSuso <suinky1999@gmail.com>
2026-04-12 05:15:47 +0200

    fix

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,16 +1,21 @@
 'use client'
 // src/components/ClientProviders.tsx
-// M8: Aggiunto SyncStatusListener per ricevere messaggi dal Service Worker
+// M8: Aggiunto SyncStatusListener per Background Sync toast
 
+import { LocaleProvider } from '@/lib/locale'
 import { ThemeProvider } from '@/lib/theme'
+import { ServiceWorkerRegistrar } from '@/components/ServiceWorkerRegistrar'
 import { SyncStatusListener } from '@/components/ui/SyncToast'
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
-      {/* M8: Ascolta i messaggi del SW per Background Sync */}
-      <SyncStatusListener />
-      {children}
+      <LocaleProvider>
+        <ServiceWorkerRegistrar />
+        {/* M8: ascolta messaggi Background Sync dal Service Worker */}
+        <SyncStatusListener />
+        {children}
+      </LocaleProvider>
     </ThemeProvider>
   )
 }
